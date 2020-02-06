ae384eba67730240854755f00f043218fb1d8bef
ericdu31 <ricou31560@gmail.com>
2020-02-06 15:03:35 +0100

    choixparnumero

--- TP3.c
+++ TP3.c
@@ -1,20 +1,26 @@
-#include <stuio.h>
-#inclue <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
 
 int main(){
+    int montagne = 0 ;
+    int grotte = 1;
+    int choixChemin;
 
 
+printf("choisit ton chemin entre la montagne et la grotte ? \n");
+
+scanf("%d",&choixChemin);
+{
+if ((choixChemin == grotte)==1)
+     { printf("vous arrivez a la grotte  \n");}
 
 
-
-
-
-
+if ((choixChemin , montagne)==0)
+    { printf("vous arrivez a la montagne  \n");}
+}
 
 return 0;
-
-
-
 }
